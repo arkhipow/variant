d78d8911a417ce4248b21cdccd48e8a6baf9d2e2
arkhipow <posmedrolder48284@gmail.com>
2025-10-07 21:16:21 +0700

    Update variant.hpp

--- variant.hpp
+++ variant.hpp
@@ -2,33 +2,30 @@
 #include <algorithm>
 #include <stdexcept>
 
-template <typename T, typename... Types>
-size_t getTypeIndex() {
-	size_t index = 0;
-
-	for (bool found : { std::is_same_v<T, Types>... }) {
-		if (found) { return index; }
-		++index;
-	}
-
-	throw (std::runtime_error("Type not found in type list"));
-}
-
-template <typename T>
-void destroy(bool condition, auto* buffer) {
-	if (condition) {
-		reinterpret_cast<T*>(buffer)->~T();
-	}
-}
-
 template <typename... Types>
 class Variant final {
 private:
-	static constexpr size_t _count = sizeof...(Types);
 	static constexpr size_t _size = std::max({ sizeof(Types)... });
 	size_t _index;
 
 	char _buffer[_size];
+
+	template <typename T, typename... Types>
+	size_t getTypeIndex() {
+		size_t index = 0;
+
+		for (bool found : { std::is_same_v<T, Types>... }) {
+			if (found) { return index; }
+			++index;
+		}
+
+		throw (std::runtime_error("Type not found in type list"));
+	}
+
+	template <typename T>
+	void destroy(auto* buffer) {
+		reinterpret_cast<T*>(buffer)->~T();
+	}
 
 public:
 	template <typename T>
@@ -36,9 +33,47 @@ public:
 		new (_buffer) T(value);
 	}
 
+	Variant(const Variant& obj) : _index(obj._index) {
+		size_t index = 0;
+		((index++ == _index ? new (_buffer) Types(*reinterpret_cast<Types*>(obj._buffer)) : void()), ...);
+	}
+
+	Variant(Variant&& obj) : _index(obj._index) {
+		size_t index = 0;
+		((index++ == _index ? new (_buffer) Types(std::move(*reinterpret_cast<Types*>(obj._buffer))) : void()), ...);
+
+		obj._index = static_cast<size_t>(-1);
+	}
+
 	~Variant() {
 		size_t index = 0;
-		(destroy<Types>(index++ == _index, _buffer), ...);
+		((index++ == _index ? destroy<Types>(_buffer) : void()), ...);
+	}
+
+	Variant& operator=(const Variant& obj) {
+		if (this == &obj) { return *this; }
+
+		this->~Variant();
+		_index = obj._index;
+
+		size_t index = 0;
+		((index++ == _index ? new (_buffer) Types(*reinterpret_cast<Types*>(obj._buffer)) : void()), ...);
+
+		return *this;
+	}
+
+	Variant& operator=(Variant&& obj) {
+		if (this == &obj) { return *this; }
+
+		_index = obj._index;
+		this->~Variant();
+
+		size_t index = 0;
+		((index++ == _index ? (new (_buffer) Types(std::move(*reinterpret_cast<Types*>(obj._buffer))), void()) : void()), ...);
+
+		obj._index = static_cast<size_t>(-1);
+
+		return *this;
 	}
 
 	template<typename T>
@@ -48,5 +83,14 @@ public:
 		}
 
 		return *reinterpret_cast<T*>(_buffer);
+	}
+
+	template <typename T>
+	bool holdsAlternative() {
+		return (std::is_same_v<T, Types> || ...);
+	}
+
+	size_t getIndex() {
+		return _index;
 	}
 };
