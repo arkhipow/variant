f8e5431532123e4e7e8b346ab16af612ebbdffdd
arkhipow <posmedrolder48284@gmail.com>
2025-11-19 18:39:39 +0700

    Update meta.hpp

--- meta.hpp
+++ meta.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <type_traits>
 
 template <typename T, typename... Args>
 struct index_of;
@@ -36,69 +37,4 @@ struct type_of<0, First, Rest...> {
 template <size_t Index>
 struct type_of<Index> {
 	static_assert(false, "Type not found");
-};
-
-
-
-template <typename... Args>
-struct type_list {};
-
-
-
-template <typename T, typename... Args>
-struct make_type_list;
-
-template <typename T, typename... Args>
-struct make_type_list<T, type_list<Args...>> {
-	using type = type_list<T, Args...>;
-};
-
-
-
-template <typename T, typename... Args>
-struct remove_type;
-
-template <typename T, typename First, typename... Rest>
-struct remove_type<T, First, Rest...> {
-	using type = typename make_type_list<First, typename remove_type<T, Rest...>::type>::type;
-};
-
-template <typename T, typename... Rest>
-struct remove_type<T, T, Rest...> {
-	using type = typename remove_type<T, Rest...>::type;
-};
-
-template <typename T>
-struct remove_type<T> {
-	using type = type_list<>;
-};
-
-
-
-template <typename... Args>
-struct unique_types;
-
-template <typename T, typename... Args>
-struct unique_types<T, Args...> {
-	using type = typename make_type_list<T, typename unique_types<typename remove_type<T, Args...>::type>::type>::type;
-};
-
-template <typename T, typename... Args>
-struct unique_types<type_list<T, Args...>> {
-	using type = typename unique_types<T, Args...>::type;
-};
-
-template <typename T>
-struct unique_types<T> {
-	using type = type_list<T>;
-};
-
-template <>
-struct unique_types<type_list<>> {
-	using type = type_list<>;
-};
-
-template <>
-struct unique_types<> {
-	using type = type_list<>;
 };
